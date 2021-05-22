35acff29876107b415ac3fd5f1e2e1a76199c3a1
kvezal <muckam49@mail.ru>
2021-05-22 19:27:10 +0300

    feat: реализована логика работы product-cart (1 модуль 4 задание)

--- 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
+++ 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-product-card',
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+  @Output()
+  public goToProduct: EventEmitter<void> = new EventEmitter<void>();
 
+  @Input()
+  public product: IProduct = {} as IProduct;
+
+  public redirectTo(): void {
+    this.goToProduct.emit();
+  }
 }
