ce971cf11329ab241a8fb7e3497d62402b1b8246
kvezal <muckam49@mail.ru>
2021-05-22 19:19:44 +0300

    feat: реализована логика работы product-cart (1 модуль 3 задание)

--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,8 @@
 import {
   Component,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy, Input, EventEmitter, Output,
 } from '@angular/core';
+import {IProduct, oneProduct} from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -9,6 +10,20 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductComponent {
+  @Output()
+  public addToCart: EventEmitter<void> = new EventEmitter<void>();
 
+  @Output()
+  public goToProduct: EventEmitter<void> = new EventEmitter<void>();
 
+  @Input()
+  public product: IProduct = {} as IProduct;
+
+  public addToBasket(): void {
+    this.addToCart.emit();
+  }
+
+  public redirectTo(): void {
+    this.goToProduct.emit();
+  }
 }
