e6fab119b6b1571b367bfee0bab2bc83070852ab
kvezal <muckam49@mail.ru>
2021-05-29 12:05:17 +0300

    разработка компонента star-rating (модуль 3 задача 2)

--- 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
+++ 3-directives/2-start-rating-component/src/app/product-card/star-rating/star-rating.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-star-rating',
   templateUrl: './star-rating.component.html',
 })
 export class StarRatingComponent {
+  @Input()
+  public feedbackRate: number = 0;
 
+  public stars: number[] = [0, 1, 2, 3, 4];
+
+  public highlight(star: number): boolean {
+    const roundedRating = Math.round(this.feedbackRate);
+    return (star + 1) <= roundedRating;
+  }
 }
