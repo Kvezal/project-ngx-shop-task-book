75521089a31a9205ff1a6d2ec14b2e30f798a776
kvezal <muckam49@mail.ru>
2021-05-29 14:35:03 +0300

    разработка компонента feedbacks (модуль 3 задача 4)

--- 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
+++ 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IFeedback} from '../../../../../../shared/mocks/3-directives/interfaces';
 
 @Component({
   selector: 'ngx-shop-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.sass'],
 })
-export class FeedbacksComponent {}
+export class FeedbacksComponent {
+  @Output()
+  public addFeedbackEvent: EventEmitter<void> = new EventEmitter<void>();
+
+  @Input()
+  public feedbacks: IFeedback[] = [];
+
+  public addNewFeedback(): void {
+    this.addFeedbackEvent.emit();
+  }
+}
