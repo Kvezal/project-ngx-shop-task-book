fecc3b54116b7700a873ffd005ddd0db436098d1
kvezal <muckam49@mail.ru>
2021-05-29 11:41:47 +0300

    разработка компонента side-menu (модуль 3 задача 1)

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICategory} from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  @Output()
+  public categoryChange: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input()
+  public categories: ICategory[] = [];
+
+  public currentName: string = null;
+
+  public redirectTo(): void {
+    this.categoryChange.emit();
+  }
+
+  public hover(subCategory: string): void {
+    this.currentName = subCategory;
+  }
+
+  public mouseLeave(): void {
+    this.currentName = null;
+  }
+}
