5257b668c13e7d485822a683476b1f7f5cd05d89
kvezal <muckam49@mail.ru>
2021-05-29 14:06:26 +0300

    разработка компонента information (модуль 3 задача 3)

--- 3-directives/3-information-component/src/app/information/information.component.ts
+++ 3-directives/3-information-component/src/app/information/information.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../../../shared/mocks/3-directives/product-information';
 
 @Component({
   selector: 'ngx-shop-information',
   templateUrl: './information.component.html',
 })
-export class InformationComponent {}
+export class InformationComponent {
+  @Output()
+  public addToCart: EventEmitter<void> = new EventEmitter<void>();
+
+  @Input()
+  public product: IProduct = null;
+
+  public isShow: boolean = false;
+
+  public addToBasket(): void {
+    this.addToCart.emit();
+  }
+
+  public show(): void {
+    this.isShow = !this.isShow;
+  }
+}
