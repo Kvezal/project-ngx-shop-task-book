c10217c50844df1e91affed0a38ed6ab2de0b55f
kvezal <muckam49@mail.ru>
2021-06-14 07:06:33 +0300

    разработка директивы category-dropdown (модуль 3 задача 5)

--- 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
+++ 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICategory} from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-category-dropdown',
   templateUrl: './category-dropdown.component.html',
 })
-export class CategoryDropdownComponent {}
+export class CategoryDropdownComponent {
+  @Output()
+  public subCategorySelectEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input()
+  public categories: ICategory[] = [];
+
+  public currentIndex: number = null;
+
+  public currentCategory = '';
+
+  public showSubCategories(index: number): void {
+    this.currentIndex = index;
+  }
+
+  public subCategorySelect(subCategoryName: string): void {
+    this.subCategorySelectEvent.emit(subCategoryName);
+  }
+}
